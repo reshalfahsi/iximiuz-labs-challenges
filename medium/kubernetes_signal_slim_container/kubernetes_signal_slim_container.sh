bda225362665d71b105e5db85dd18aa8df194570
reshalfahsi <resha.alfahsi@gmail.com>
2025-06-18 11:04:53 +0700

    Update Send a Signal to a Kubernetes App: a Slim Container Case

--- medium/kubernetes_signal_slim_container/kubernetes_signal_slim_container.sh
+++ medium/kubernetes_signal_slim_container/kubernetes_signal_slim_container.sh
@@ -1,1 +1,13 @@
 #!/usr/bin/env bash
+
+# Starts an interactive shell in a new ephemeral alpine container in 
+# the slim pod, which is attached to the app container's 
+# uts, ipc, net, and pid namespaces.
+#
+# pid namespace sharing, enabled by the --target option
+kubectl debug -it \
+    --image ghcr.io/iximiuz/labs/alpine:3 \
+    --target app \
+    slim
+
+kill -SIGUSR1 $(pgrep server)
